2c4417922257e5d9496a6fec2ed33c2363ebe281
yahya serhan <yahyaserhanmoon@gmail.com>
2025-03-03 21:48:35 +0300

    add db backup folder

--- LibraryMangmentSystem/obj/Release/net8.0-windows/LibraryMangmentSystem.AssemblyInfo.cs
+++ LibraryMangmentSystem/obj/Release/net8.0-windows/LibraryMangmentSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryMangmentSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b82be74ce68e41ab1c16d92c942c9404fb0cd77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7908fee62cefbb0c341543929017021c0b116c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryMangmentSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryMangmentSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
